6b95dde7a9eb510215a7dc6bd70e723bc5400be9
Anders Hafreager <anders@hafreager.no>
2014-11-22 15:30:53 +0100

    Added ProgressiveDownloader class

---
+++ framework/Source/ProgressiveDownloader.h
@@ -0,0 +1,13 @@
+//
+//  ProgressiveDownloader.h
+//  GPUImage
+//
+//  Created by Anders Hafreager on 22/11/14.
+//  Copyright (c) 2014 Brad Larson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ProgressiveDownloader : NSObject
+
+@end
---
+++ framework/Source/ProgressiveDownloader.m
@@ -0,0 +1,13 @@
+//
+//  ProgressiveDownloader.m
+//  GPUImage
+//
+//  Created by Anders Hafreager on 22/11/14.
+//  Copyright (c) 2014 Brad Larson. All rights reserved.
+//
+
+#import "ProgressiveDownloader.h"
+
+@implementation ProgressiveDownloader
+
+@end
